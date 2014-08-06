3655d0d3e075b5ea2f62b3e3a6cb4cd97aeddecc
Eugene van der Merwe <eugenevdm@gmail.com>
2014-08-06 16:28:10 +0200

    Minor type fixing

--- app/code/community/PayNow/PayNow/controllers/NotifyController.php
+++ app/code/community/PayNow/PayNow/controllers/NotifyController.php
@@ -4,8 +4,8 @@
  */
 
 // Include the Sage Pay Now common file
-define( 'PN_DEBUG', ( Mage::getStoreConfig( 'payment/paynow/debugging' ) ? true : false ) );
-include_once( dirname( __FILE__ ) .'/../paynow_common.inc' );
+define('PN_DEBUG', (Mage::getStoreConfig('payment/paynow/debugging') ? true : false));
+include_once(dirname(__FILE__) . '/../paynow_common.inc');
 
 
 /**
@@ -14,11 +14,11 @@ include_once( dirname( __FILE__ ) .'/../paynow_common.inc' );
 class PayNow_PayNow_NotifyController extends Mage_Core_Controller_Front_Action
 {
     // {{{ indexAction()
-	/**
-	 * indexAction
-     * 
+    /**
+     * indexAction
+     *
      * Instantiate IPN model and pass IPN request to it
-	 */
+     */
     public function indexAction()
     {
         // Variable Initialization
@@ -26,120 +26,111 @@ class PayNow_PayNow_NotifyController extends Mage_Core_Controller_Front_Action
         $pnErrMsg = '';
         $pnData = array();
 
-        pnlog( 'Sage Pay Now IPN call received' );
-        pnlog( 'Server = '. Mage::getStoreConfig( 'payment/paynow/server' ) );
-        
+        pnlog('Sage Pay Now IPN call received');
+        pnlog('Server = ' . Mage::getStoreConfig('payment/paynow/server'));
+
         // Notify Pay Now that information has been received
-        if( !$pnError )
-        {
-            header( 'HTTP/1.0 200 OK' );
+        if (!$pnError) {
+            header('HTTP/1.0 200 OK');
             flush();
         }
-        
+
         // Get data posted back by Pay Now
-        if( !$pnError )
-        {
-            pnlog( 'Get posted data' );
-        
+        if (!$pnError) {
+            pnlog('Get posted data');
+
             // Posted variables from ITN
             $pnData = pnGetData();
-        
-            pnlog( 'Sage Pay Now Data: '. print_r( $pnData, true ) );
-        
-            if( $pnData === false )
-            {
+
+            pnlog('Sage Pay Now Data: ' . print_r($pnData, true));
+
+            if ($pnData === false) {
                 $pnError = true;
                 $pnErrMsg = PN_ERR_BAD_ACCESS;
             }
         }
-        
-		if ($pnData['TransactionAccepted'] == 'false') {
-			$pnError = true;
-			// TODO PN_DECLINED does not exist
-			//$pnErrMsg = PN_DECLINED			
-		}
-        
+
+        if ($pnData['TransactionAccepted'] == 'false') {
+            $pnError = true;
+            $pnErrMsg = PN_MSG_FAILED;
+        }
+
         // Get internal order and verify it hasn't already been processed
-        if( !$pnError )
-        {
-            pnlog( "Check order hasn't been processed" );
-            
+        if (!$pnError) {
+            pnlog("Check if the order has not already been processed");
+
             // Load order
-    		$trnsOrdId = $pnData['Reference'];
-    		$order = Mage::getModel( 'sales/order' );
-            $order->loadByIncrementId( $trnsOrdId );
-    		$this->_storeID = $order->getStoreId();
-            
+            $trnsOrdId = $pnData['Reference'];
+            $order = Mage::getModel('sales/order');
+            $order->loadByIncrementId($trnsOrdId);
+            $this->_storeID = $order->getStoreId();
+
             // Check order is in "pending payment" state
-            if( $order->getStatus() !== Mage_Sales_Model_Order::STATE_PENDING_PAYMENT )
-            {
+            if ($order->getStatus() !== Mage_Sales_Model_Order::STATE_PENDING_PAYMENT) {
                 $pnError = true;
-                $pnErrMsg = PF_ERR_ORDER_PROCESSED;
+                $pnErrMsg = PN_ERR_ORDER_PROCESSED;
             }
         }
-        
+
         // Check status and update order
-        if( !$pnError )
-        {
-            pnlog( 'Check status and update order' );
-            
+        if (!$pnError) {
+            pnlog('Check status and update order');
+
             // Successful
-            if( $pnData['TransactionAccepted'] == "true" )
-            {
-                pnlog( 'Order complete' );
-                
+            if ($pnData['TransactionAccepted'] == "true") {
+                pnlog('Order complete');
+
                 // Update order additional payment information
-                $payment = $order->getPayment(); 
-        		$payment->setAdditionalInformation( "TransactionAccepted", $pnData['TransactionAccepted'] );
-        		$payment->setAdditionalInformation( "Reference", $pnData['Reference'] );
-                $payment->setAdditionalInformation( "RequestTrace", $pnData['RequestTrace'] );
+                $payment = $order->getPayment();
+                $payment->setAdditionalInformation("TransactionAccepted", $pnData['TransactionAccepted']);
+                $payment->setAdditionalInformation("Reference", $pnData['Reference']);
+                $payment->setAdditionalInformation("RequestTrace", $pnData['RequestTrace']);
                 //$payment->setAdditionalInformation( "email_address", $pnData['email_address'] );
-        		$payment->setAdditionalInformation( "Amount", $pnData['Amount'] );
+                $payment->setAdditionalInformation("Amount", $pnData['Amount']);
                 $payment->save();
 
                 // Save invoice
-                $this->saveInvoice( $order );
+                $this->saveInvoice($order);
             }
         }
-        
+
         // If an error occurred
-        if( $pnError )
-        {
-            pnlog( 'Error occurred: '. $pnErrMsg );
-            pnlog( 'Reason: '. $pnData['Reason'] );
-            $url = Mage::getUrl( 'paynow/redirect/cancel', array( '_secure' => true ) );
+        if ($pnError) {
+            pnlog('Error occurred: ' . $pnErrMsg);
+            pnlog('Reason: ' . $pnData['Reason']);
+            $url = Mage::getUrl('paynow/redirect/cancel', array('_secure' => true));
             echo "Transaction failed, reason: " . $pnData['Reason'] . "<br><br>";
             echo "<a href='$url'>Click here to return to the store.</a>";
             // TODO: Use Magento structures to send email
         } else {
-        	// return Mage::getUrl( 'paynow/redirect/success', array( '_secure' => true ) );
-        	$this->_redirect('paynow/redirect/success');
-        	
-        }        
+            // return Mage::getUrl( 'paynow/redirect/success', array( '_secure' => true ) );
+            $this->_redirect('paynow/redirect/success');
+
+        }
 
     }
 
     /**
-	 * saveInvoice
-	 */
-	protected function saveInvoice( Mage_Sales_Model_Order $order )
+     * saveInvoice
+     */
+    protected function saveInvoice(Mage_Sales_Model_Order $order)
     {
-        pnlog( 'Saving invoice' );
-        
-		// Check for mail msg
-		$invoice = $order->prepareInvoice();
+        pnlog('Saving invoice');
 
-		$invoice->register()->capture();
-		Mage::getModel( 'core/resource_transaction' )
-		   ->addObject( $invoice )
-		   ->addObject( $invoice->getOrder() )
-		   ->save();
-		//$invoice->sendEmail();
-		
-		$message = Mage::helper( 'paynow' )->__( 'Notified customer about invoice #%s.', $invoice->getIncrementId() );
-        $comment = $order->sendNewOrderEmail()->addStatusHistoryComment( $message )
-              ->setIsCustomerNotified( true )
-              ->save();
+        // Check for mail msg
+        $invoice = $order->prepareInvoice();
+
+        $invoice->register()->capture();
+        Mage::getModel('core/resource_transaction')
+            ->addObject($invoice)
+            ->addObject($invoice->getOrder())
+            ->save();
+        //$invoice->sendEmail();
+
+        $message = Mage::helper('paynow')->__('Notified customer about invoice #%s.', $invoice->getIncrementId());
+        $comment = $order->sendNewOrderEmail()->addStatusHistoryComment($message)
+            ->setIsCustomerNotified(true)
+            ->save();
     }
 
 }
