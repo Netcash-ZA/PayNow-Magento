6e2236e7a82ab453f4002a056ce0857fad3f81fe
Jurgens Banninga <jurgens@interexcel.co.za>
2015-08-19 16:26:24 +0200

    Add new fields

--- app/code/community/PayNow/PayNow/Model/Standard.php
+++ app/code/community/PayNow/PayNow/Model/Standard.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Standard.php 
+ * Standard.php
  */
 
 /**
@@ -12,7 +12,7 @@ class PayNow_PayNow_Model_Standard extends Mage_Payment_Model_Method_Abstract
 	protected $_formBlockType = 'paynow/form';
 	protected $_infoBlockType = 'paynow/payment_info';
 	protected $_order;
-	
+
 	protected $_isGateway              = true;
 	protected $_canAuthorize           = true;
 	protected $_canCapture             = true;
@@ -127,12 +127,12 @@ class PayNow_PayNow_Model_Standard extends Mage_Payment_Model_Method_Abstract
         $orderIncrementId = $this->getCheckout()->getLastRealOrderId();
         $order = Mage::getModel( 'sales/order' )->loadByIncrementId( $orderIncrementId );
 		$description = '';
-		
+
 		// Sage Pay Now service key
         $serviceKey = $this->getConfigData( 'service_key' );
         // Sage Pay Now software vendor key
         $softwareVendorKey = '24ade73c-98cf-47b3-99be-cc7b867b3080';
-        
+
         // Create description
         foreach( $order->getAllItems() as $items )
         {
@@ -145,28 +145,37 @@ class PayNow_PayNow_Model_Standard extends Mage_Payment_Model_Method_Abstract
 		}
 		$description .= 'Shipping = '. $order->getOrderCurrencyCode() . $this->getNumberFormat( $order->getShippingAmount() ).';';
 		$description .= 'Total = '. $order->getOrderCurrencyCode() . $this->getTotalAmount( $order ).';';
-		
+
+        $customerName = $order->getData('customer_firstname') . " " . $order->getData('customer_lastname');
+        $orderID = $this->getRealOrderId();
+        $customerID = $order->getData('customer_id');
+        $sageGUID = "TBC";
+
         // Construct data for the form
 		$data = array(
             // Merchant details
-            
+
 			'm1' => $serviceKey,
 			'm2' => $softwareVendorKey,
 			'return_url' => $this->getPaidSuccessUrl(),
 			'cancel_url' => $this->getPaidCancelUrl(),
 			'notify_url' => $this->getPaidNotifyUrl(),
-			
+
             // Buyer details
 			// M9 = cardholder
 			'm9' => $order->getData( 'customer_email' ),
-            
+
+            'p3' => "{$customerName} | {$orderID}",
+            'm3' => "$sageGUID",
+            'm4' => "{$customerID}",
+
             // Item details
             // P3 = description
-			'p3' => $this->getStoreName().', Order #'.$this->getRealOrderId(),			
+			// 'p3' => $this->getStoreName().', Order #'.$this->getRealOrderId(),
 			'p4' => $this->getTotalAmount( $order ),
 			// p2 = unique ref
-			'p2' => $this->getRealOrderId()            
-            
+			'p2' => $this->getRealOrderId()
+
         );
         pnlog ('Standard.php/getStandardCheckoutFormFields: ' . print_r($data,true));
 		return( $data );
@@ -185,12 +194,12 @@ class PayNow_PayNow_Model_Standard extends Mage_Payment_Model_Method_Abstract
 
     /**
      * getPayNowUrl
-     * 
+     *
      * Get URL for form submission to Sage Pay Now.
      */
 	public function getPayNowUrl()
     {
-		$url = 'https://paynow.sagepay.co.za/site/paynow.aspx';        
+		$url = 'https://paynow.sagepay.co.za/site/paynow.aspx';
 		return( $url );
     }
 
